3e1c3486fcd9e0dc3ea72cf372bfc3a8fe5471e2
Klyaker <89944083+Klyaker@users.noreply.github.com>
2022-06-05 15:39:53 +0200

    almost

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,36 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+let modalover = document.querySelector('.modaloverlay');
+let buttonOpen = document.querySelector('.btn');
+let buttonClose = document.querySelector('.modaloverlay');
+let modalmain = document.querySelector('.mainmodal');
+
+
+
+const openModal = () => {
+    buttonOpen.addEventListener('click', (e) => {
+        modalover.classList.add('active');
+    });
+};
+
+
+buttonOpen.addEventListener('click', openModal)
+
+const close = () => {
+    modalover.classList.remove('active');
+};
+
+buttonClose.addEventListener('click', close);
+
+let pushbtn = 0;
+
+function increment() {
+    pushbtn += 1;
+    modalmain.innerHTML += '<h1>1</h1>';
+}
+
+buttonOpen.addEventListener('click', increment);
+
